6cff71ef6937bf3eeb3610fc455514a6f4548606
microjan <1227971544@qq.com>
2021-04-07 18:34:33 +0800

    update

--- test.sh
+++ test.sh
@@ -3,16 +3,17 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+
 if [ -n "$1" ] ;then
     msg="$1"
 else
     msg='update'
 fi
+# if [ ! -n "$1" ] ;then
+#      msg=$1
+# else
+#    msg='update'
+# fi
 
-git init
-git add -A
-git commit -m $msg
-
-git push origin master
-
-cd -
+echo $msg
